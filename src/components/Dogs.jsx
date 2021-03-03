11f2cc7d85e9b5137fc7adbafb3651afb0bb0241
LeonarDev <leonardo.majevski@gmail.com>
2021-03-03 00:16:47 -0300

    Starts project and adds api request

---
+++ src/components/Dogs.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+class Dogs extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      dogObj: undefined,
+      loading: true,
+      storedDogs: [],
+    };
+  }
+
+  fetchDog = async () => {
+    const requestReturn = await fetch('https://dog.ceo/api/breeds/image/random');
+    const requestObject = await requestReturn.json();
+    this.setState({
+      dogObj: requestObject
+    })
+  }
+
+  componentDidMount() {
+    this.fetchDog()
+  }
+
+  saveDog = () => {
+
+  }
+
+  renderDogElement = () => {
+    return (
+      <div>
+        <p>{ this.state.dogObj.message }</p>
+        <button type="button" onClick={ this.saveDog }>Salvar Dog!</button>
+      </div>
+    )
+  }
+
+  render() {
+
+    const { storedDogs } = this.state;
+    const loadingElement = <span>Loading...</span>;
+
+    return (
+      <div>
+        <span>
+          { storedDogs.map(({ message }) => (<p key={ message }>{ message }</p>)) }
+        </span>
+
+        <span>RENDERIZAÇÂO CONDICIONAL</span>
+      </div>
+    )
+  }
+}
+
+export default Dogs;
